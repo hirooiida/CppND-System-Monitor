96561c8492d03b1c46fd44e163f5ff6949bfcb07
hirooiida <mail@mail.com>
2020-09-27 05:22:05 +0000

    Corrected a typo

--- src/system.cpp
+++ src/system.cpp
@@ -20,7 +20,7 @@ Processor& System::Cpu() { return cpu_; }
 // TODO: Return a container composed of the system's processes
 vector<Process>& System::Processes() { return processes_; }
 
-// TODO: Return the system's kernel identifier (string)
+// DONE: Return the system's kernel identifier (string)
 std::string System::Kernel() { return LinuxParser::Kernel(); }
 
 // TODO: Return the system's memory utilization
